3dfe3cf2209847cd89a6c1b7a464c3c104e3861c
Daniela Rique <drique@asesoftware.com>
2023-06-05 12:18:46 -0500

    Avances curso clase 13

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+//import App from './App/index.js';
+import "./index.css";
 
-ReactDOM.render(
+function App(props) {
+  return (
+    <h1>
+      !{props.saludo}! {props.nombre}
+    </h1>
+  );
+}
+function withSaludo(WrappedComponent) {
+  return function wrappedComponentWithSaludo(saludo){
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <p>Estamos acompañando el WrappedComponent</p>
+        </React.Fragment>
+      );
+    };
+  }
+}
+/* ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+); */
+
+const AppWithSaludo = withSaludo(App)('Wuenas');
+ReactDOM.render(<AppWithSaludo  nombre="dani" />,  document.getElementById('root'));
